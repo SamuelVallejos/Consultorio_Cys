184031e357a825458b47d5d214c7d5585f0e3d75
javierrojas12 <Ja.rojast@duocuc.cl>
2024-09-29 00:54:53 -0300

    vista registro cambiada

--- Web/consultorioCys/views.py
+++ Web/consultorioCys/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login  # Importa y renombra la función
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import logout
+from .forms import CustomUserCreationForm
 
 def inicio(request):
     return render(request, 'consultorioCys/inicio.html')
@@ -26,13 +27,13 @@ def login_view(request):
 
 def register_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()  # Guarda el usuario en la base de datos
-            auth_login(request, user)  # Autentica al usuario automáticamente después del registro
+            user = form.save()  # Guardar el usuario
+            auth_login(request, user)  # Autenticar al usuario después del registro
             return redirect('inicio')  # Redirige a la página de inicio
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'consultorioCys/register.html', {'form': form})
 def logout_view(request):
     logout(request)
